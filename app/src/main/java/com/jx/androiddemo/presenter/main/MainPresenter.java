d15e75915af6ec1737c37ff18a651fcaec3f692f
jx <jixiao@guiji.ai>
2021-09-29 17:53:53 +0800

    梯形

--- app/src/main/java/com/jx/androiddemo/presenter/main/MainPresenter.java
+++ app/src/main/java/com/jx/androiddemo/presenter/main/MainPresenter.java
@@ -35,6 +35,7 @@ import com.jx.androiddemo.testactivity.ui.u17.U17Activity;
 import com.jx.androiddemo.testactivity.ui.u18.U18Activity;
 import com.jx.androiddemo.testactivity.ui.u19.U19Activity;
 import com.jx.androiddemo.testactivity.ui.u2.U2Activity;
+import com.jx.androiddemo.testactivity.ui.u20.U20Activity;
 import com.jx.androiddemo.testactivity.ui.u3.U3Activity;
 import com.jx.androiddemo.testactivity.ui.u4.U4Activity;
 import com.jx.androiddemo.testactivity.ui.u5.U5Activity;
@@ -112,6 +113,7 @@ public class MainPresenter extends BaseRxPresenter<MainContract.View> implements
             mMainPageList.add(new MainPageBean("选择日期", U17Activity.class));
             mMainPageList.add(new MainPageBean("选择日期2", U18Activity.class));
             mMainPageList.add(new MainPageBean("Bezier曲线", U19Activity.class));
+            mMainPageList.add(new MainPageBean("梯形", U20Activity.class));
 
 
         }
