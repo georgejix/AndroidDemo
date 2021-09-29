b003f640d25956a4386d27a5049e11dce4fd8b4f
jx <jixiao@guiji.ai>
2021-09-29 11:15:09 +0800

    http

--- app/src/main/java/com/jx/androiddemo/testactivity/function/f12/F12Activity.java
+++ app/src/main/java/com/jx/androiddemo/testactivity/function/f12/F12Activity.java
@@ -1,6 +1,11 @@
 package com.jx.androiddemo.testactivity.function.f12;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
 
 import com.jx.androiddemo.BaseMvpActivity;
 import com.jx.androiddemo.R;
@@ -9,10 +14,20 @@ import com.jx.androiddemo.testactivity.function.empty.EmptyPresenter;
 
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class F12Activity extends BaseMvpActivity<EmptyPresenter> implements EmptyContract.View {
+    private final String TAG = "HttpActivity";
+
+    @BindView(R.id.img_1)
+    ImageView img1;
+
+    private ViewHandler viewHandler;
+    private Bitmap bitmap;
+
+    private final int REFRESH_IMG = 1001;
 
     @Override
     protected void initInject() {
@@ -32,6 +47,30 @@ public class F12Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
     }
 
     private void initView() {
+        if (null == viewHandler) {
+            viewHandler = new ViewHandler();
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //String s = HttpUtils.getInstance().sendGetMessage("http://pic1.cxtuku.com/00/06/78/b9903ad9ea2b.jpg", "");
+                byte b[] = HttpURLConnectionUtil.getInstance().sendGet("http://pic1.cxtuku.com/00/06/78/b9903ad9ea2b.jpg");
+                if (null != b) {
+                    bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+                }
+                if (null != viewHandler) {
+                    viewHandler.sendEmptyMessage(REFRESH_IMG);
+                }
+
+                /*Map<String, String> params = new HashMap<String, String>();
+                        params.put("username", "getOrderRecord");
+                        params.put("password", "122223");
+                String s = HttpURLConnectionUtil.getInstance().sendPostMessage("https://passport.baidu.com/v2/api/?login", params, "utf-8");
+                if(null != s){
+                    Log.d(TAG, s);
+                }*/
+            }
+        }).start();
     }
 
     @SuppressLint("CheckResult")
@@ -51,5 +90,19 @@ public class F12Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
                 .subscribe(o ->
                 {
                 });*/
+    }
+
+    class ViewHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case REFRESH_IMG:
+                    if (null != img1 && null != bitmap) {
+                        img1.setImageBitmap(bitmap);
+                    }
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/jx/androiddemo/testactivity/ui/u14/U14Activity.java
+++ app/src/main/java/com/jx/androiddemo/testactivity/ui/u14/U14Activity.java
@@ -1,6 +1,22 @@
 package com.jx.androiddemo.testactivity.ui.u14;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 
 import com.jakewharton.rxbinding2.view.RxView;
 import com.jx.androiddemo.BaseMvpActivity;
@@ -8,14 +24,23 @@ import com.jx.androiddemo.R;
 import com.jx.androiddemo.constant.Constants;
 import com.jx.androiddemo.testactivity.function.empty.EmptyContract;
 import com.jx.androiddemo.testactivity.function.empty.EmptyPresenter;
+import com.jx.androiddemo.testactivity.ui.u4.Utils;
 
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class U14Activity extends BaseMvpActivity<EmptyPresenter> implements EmptyContract.View {
+    private final String TAG = "TestSomeViewActivity";
 
+    @BindView(R.id.textview_one)
+    TextView oneTextView;
+    @BindView(R.id.textview_two)
+    TextView twoTextView;
+    @BindView(R.id.textview_three)
+    MTextView threeTextView;
     @Override
     protected void initInject() {
         getActivityComponent().inject(this);
@@ -34,6 +59,33 @@ public class U14Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
     }
 
     private void initView() {
+        //设置不同颜色
+        String textOne = oneTextView.getText().toString();
+        SpannableStringBuilder sp = new SpannableStringBuilder(textOne);
+        sp.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.black)),
+                0,
+                textOne.length(),
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        sp.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.red)),
+                3,
+                11,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        oneTextView.setText(sp);
+
+        //设置不同字体
+        String textTwo = twoTextView.getText().toString();
+        SpannableStringBuilder spTwo = new SpannableStringBuilder(textTwo);
+        spTwo.setSpan(new AbsoluteSizeSpan(Utils.sp2px(this, 19)),
+                3,
+                11,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        twoTextView.setText(spTwo);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            addButton();
+        }
+        //threeTextView.setText(stringFilter(ToDBC(getResources().getString(R.string.alarm_service_contract))));
+        //threeTextView.setText(ToSBC(getResources().getString(R.string.alarm_service_contract)));
+        threeTextView.setMText(getResources().getString(R.string.alarm_service_contract));
     }
 
     @SuppressLint("CheckResult")
@@ -53,5 +105,44 @@ public class U14Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
                 .subscribe(o ->
                 {
                 });*/
+    }
+
+    private WindowManager windowManager;
+    private Button button;
+    //通过windowmanager添加一个view到window，可以通过type设置z轴优先级（应用window级：1-99/子window级：1000-1999/系统window级：2000-2999）
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void addButton(){
+        //判断是否有 允许显示在其他应用的上层 的权限
+        if (! Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent,10);
+        }else {
+            windowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+            button = new Button(this);
+            button.setText("12345");
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//6.0
+                params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            } else {
+                params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+            }
+            params.format = PixelFormat.TRANSLUCENT;// 支持透明
+            params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+            params.gravity = Gravity.TOP | Gravity.LEFT;
+            params.x = 100;
+            params.y = 300;
+            params.width = 100;
+            params.height = 100;
+            windowManager.addView(button, params);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(null != button && null != windowManager){
+            windowManager.removeView(button);
+        }
     }
 }
