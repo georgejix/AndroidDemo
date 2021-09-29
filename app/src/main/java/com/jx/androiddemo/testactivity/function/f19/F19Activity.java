73c123c14971cac51441605eb9b78ba5e880ae64
jx <jixiao@guiji.ai>
2021-09-29 17:58:28 +0800

    handler

--- app/src/main/java/com/jx/androiddemo/testactivity/function/f19/F19Activity.java
+++ app/src/main/java/com/jx/androiddemo/testactivity/function/f19/F19Activity.java
@@ -1,6 +1,11 @@
 package com.jx.androiddemo.testactivity.function.f19;
 
 import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 
 import com.jakewharton.rxbinding2.view.RxView;
 import com.jx.androiddemo.BaseMvpActivity;
@@ -11,10 +16,17 @@ import com.jx.androiddemo.testactivity.function.empty.EmptyPresenter;
 
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class F19Activity extends BaseMvpActivity<EmptyPresenter> implements EmptyContract.View {
+    private final String TAG = "TestHandlerActivity2";
+
+    @BindView(R.id.handler1)
+    View view_handler1;
+    @BindView(R.id.handler2)
+    View view_handler2;
 
     @Override
     protected void initInject() {
@@ -31,6 +43,7 @@ public class F19Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
     protected void initEventAndData() {
         initView();
         initListener();
+        testHandler();
     }
 
     private void initView() {
@@ -47,11 +60,44 @@ public class F19Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
                 });
 
         //点击
-        /*RxView.clicks(null)
+        RxView.clicks(view_handler1)
                 .throttleFirst(Constants.CLICK_TIME, TimeUnit.MILLISECONDS)
                 .compose(this.bindToLifecycle())
                 .subscribe(o ->
                 {
-                });*/
+                    if (null != handler1) {
+                        handler1.sendEmptyMessage(1001);
+                    }
+                });
+        RxView.clicks(view_handler2)
+                .throttleFirst(Constants.CLICK_TIME, TimeUnit.MILLISECONDS)
+                .compose(this.bindToLifecycle())
+                .subscribe(o ->
+                {
+                    if (null != handler1) {
+                        handler1.sendEmptyMessage(1002);
+                    }
+                });
+    }
+
+    Handler handler1;
+
+    private void testHandler() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                handler1 = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        super.handleMessage(msg);
+                        Log.d(TAG, msg.what + "");
+                        Looper.myLooper().quit();
+                    }
+                };
+                Looper.loop();
+                Log.d(TAG, "loop end");
+            }
+        }).start();
     }
 }
