d7c2c536e9515918bae3364ac3298031a78657bb
jx <jixiao@guiji.ai>
2021-09-30 11:56:08 +0800

    整理代码

--- app/src/main/java/com/jx/androiddemo/BaseMvpActivity.java
+++ app/src/main/java/com/jx/androiddemo/BaseMvpActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import com.jx.arch.util.DeviceUtils;
-import com.jx.arch.util.StatusBarUtil;
-import com.trello.rxlifecycle2.LifecycleTransformer;
 import com.jx.androiddemo.di.component.ActivityComponent;
 import com.jx.androiddemo.di.component.DaggerActivityComponent;
 import com.jx.androiddemo.di.module.ActivityModule;
@@ -15,6 +12,9 @@ import com.jx.androiddemo.event.NoticeEvent;
 import com.jx.androiddemo.presenter.BasePresenter;
 import com.jx.androiddemo.presenter.BaseRxPresenter;
 import com.jx.androiddemo.tool.ToastUtil;
+import com.jx.arch.util.DeviceUtils;
+import com.jx.arch.util.StatusBarUtil;
+import com.trello.rxlifecycle2.LifecycleTransformer;
 
 import javax.inject.Inject;
 
@@ -24,8 +24,7 @@ import butterknife.Unbinder;
 /**
  * Created by WangTao
  */
-public abstract class BaseMvpActivity<T extends BasePresenter> extends BaseAppActivity implements BaseView
-{
+public abstract class BaseMvpActivity<T extends BasePresenter> extends BaseAppActivity implements BaseView {
     @Inject
     protected T mPresenter;
 
@@ -33,8 +32,9 @@ public abstract class BaseMvpActivity<T extends BasePresenter> extends BaseAppAc
 
     private Unbinder mUnBinder;
 
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    public String TAG = getClass().getSimpleName();
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayout());
         mUnBinder = ButterKnife.bind(this);
@@ -42,59 +42,49 @@ public abstract class BaseMvpActivity<T extends BasePresenter> extends BaseAppAc
         //setStatusBarColor(getResources().getColor(R.color.head_view_bg));
         mContext = this;
         initInject();
-        if (mPresenter != null)
-        {
+        if (mPresenter != null) {
             mPresenter.attachView(this);
         }
         initEventAndData();
     }
 
     @Override
-    public <T> LifecycleTransformer<T> bindToLife()
-    {
+    public <T> LifecycleTransformer<T> bindToLife() {
         return this.bindToLifecycle();
     }
 
     @Override
-    protected void onStart()
-    {
+    protected void onStart() {
         super.onStart();
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         DeviceUtils.NavigationBarStatusBar(getWindow());
     }
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         DeviceUtils.NavigationBarStatusBar(getWindow());
     }
 
     @Override
-    protected void onDestroy()
-    {
+    protected void onDestroy() {
         super.onDestroy();
-        if (mPresenter != null)
-        {
+        if (mPresenter != null) {
             mPresenter.detachView();
         }
-        if (mUnBinder != null)
-        {
+        if (mUnBinder != null) {
             mUnBinder.unbind();
         }
-        if (mPresenter != null && mPresenter instanceof BaseRxPresenter)
-        {
+        if (mPresenter != null && mPresenter instanceof BaseRxPresenter) {
             ((BaseRxPresenter) mPresenter).doDispose();
         }
     }
 
-    protected void setStatusBarColor(int color)
-    {
+    protected void setStatusBarColor(int color) {
         StatusBarUtil.setColor(this, color, 0);
     }
 
@@ -107,37 +97,31 @@ public abstract class BaseMvpActivity<T extends BasePresenter> extends BaseAppAc
 
     protected abstract void initEventAndData();
 
-    protected ActivityComponent getActivityComponent()
-    {
+    protected ActivityComponent getActivityComponent() {
         return DaggerActivityComponent.builder()
                 .appComponent(BaseApplication.getAppComponent())
                 .activityModule(getActivityModule())
                 .build();
     }
 
-    protected ActivityModule getActivityModule()
-    {
+    protected ActivityModule getActivityModule() {
         return new ActivityModule(this);
     }
 
     @Override
-    public void showMsg(String msg)
-    {
+    public void showMsg(String msg) {
         ToastUtil.showTextToast(msg);
     }
 
     @Override
-    public void showProgressDialog(int tag, boolean canCancel)
-    {
+    public void showProgressDialog(int tag, boolean canCancel) {
     }
 
     @Override
-    public void hiddenProgressDialog()
-    {
+    public void hiddenProgressDialog() {
     }
 
     @Override
-    public void onEventMainThread(NoticeEvent event)
-    {
+    public void onEventMainThread(NoticeEvent event) {
     }
 }
