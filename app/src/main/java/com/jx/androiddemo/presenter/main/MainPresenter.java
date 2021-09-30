791afccb56ee6cb498d94e1626f61172c548522d
jx <jixiao@guiji.ai>
2021-09-30 10:02:43 +0800

    loading

--- app/src/main/java/com/jx/androiddemo/presenter/main/MainPresenter.java
+++ app/src/main/java/com/jx/androiddemo/presenter/main/MainPresenter.java
@@ -42,6 +42,7 @@ import com.jx.androiddemo.testactivity.ui.u19.U19Activity;
 import com.jx.androiddemo.testactivity.ui.u2.U2Activity;
 import com.jx.androiddemo.testactivity.ui.u20.U20Activity;
 import com.jx.androiddemo.testactivity.ui.u21.U21Activity;
+import com.jx.androiddemo.testactivity.ui.u22.U22Activity;
 import com.jx.androiddemo.testactivity.ui.u3.U3Activity;
 import com.jx.androiddemo.testactivity.ui.u4.U4Activity;
 import com.jx.androiddemo.testactivity.ui.u5.U5Activity;
@@ -126,6 +127,7 @@ public class MainPresenter extends BaseRxPresenter<MainContract.View> implements
             mMainPageList.add(new MainPageBean("Bezier曲线", U19Activity.class));
             mMainPageList.add(new MainPageBean("梯形", U20Activity.class));
             mMainPageList.add(new MainPageBean("事件分发", U21Activity.class));
+            mMainPageList.add(new MainPageBean("loading", U22Activity.class));
 
 
         }
