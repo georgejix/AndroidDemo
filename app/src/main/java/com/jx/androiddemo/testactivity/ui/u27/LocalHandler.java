03db3bdfab1fd9e3e31e4bbee8e09272abe32792
jx <jixiao@guiji.ai>
2021-11-19 11:14:41 +0800

    悬浮窗口

---
+++ app/src/main/java/com/jx/androiddemo/testactivity/ui/u27/LocalHandler.java
@@ -0,0 +1,27 @@
+package com.jx.androiddemo.testactivity.ui.u27;
+
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+public class LocalHandler extends Handler {
+    private final Listener mListener;
+
+    public LocalHandler(Looper looper, Listener listener) {
+        super(looper);
+        mListener = listener;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+        if (null != mListener) {
+            mListener.handleMsg(msg);
+        }
+    }
+
+    public interface Listener {
+        void handleMsg(Message msg);
+    }
+}
