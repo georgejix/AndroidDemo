86ddd1b6d9f41cc03176bcfb2b3f77476f11d478
OF3492 <jixiao@qianmi.com>
2022-01-28 20:33:49 +0800

    .

--- app/src/test/java/com/jx/normal/比对两句话.java
+++ app/src/test/java/com/jx/normal/比对两句话.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class 比对两句话 {
+public class 比对两句话
+{
     @Test
-    public void main() {
+    public void main()
+    {
         String str1 = "晚饭后出去散步，走着走着又到了这里。";
-        String str2 = "晚饭前,不去散步，走着走着又到了。";
+        String str2 = "晚饭前,不去散步，走着走着又到了这里。";
         List<Integer> mark = compareTwoStr(str1, str2);
-        for (int i = 0; i < mark.size(); i = i + 2) {
+        for (int i = 0; i < mark.size(); i = i + 2)
+        {
             System.out.println(mark.get(i) + "-" + mark.get(i + 1));
         }
     }
@@ -23,36 +26,51 @@ public class 比对两句话 {
      * @param str2
      * @return
      */
-    public static List<Integer> compareTwoStr(String str1, String str2) {
+    public static List<Integer> compareTwoStr(String str1, String str2)
+    {
         List<Integer> mark = new ArrayList<>();
         String str1WithoutSymbol = removeSymbol(str1);
         String str2WithoutSymbol = removeSymbol(str2);
         int symbolCount = 0;
         char[] str1Char = str1.toCharArray();
         char[] str2Char = str2WithoutSymbol.toCharArray();
-        for (int i = 0; i < str1Char.length; i++) {
-            if ("".equals(removeSymbol(String.valueOf(str1Char[i])))) {
+        for (int i = 0; i < str1Char.length; i++)
+        {
+            if ("".equals(removeSymbol(String.valueOf(str1Char[i]))))
+            {
                 symbolCount++;
                 continue;
             }
-            if (str1Char[i] != str2Char[i - symbolCount]) {
-                if (0 == mark.size() % 2) {
-                    mark.add(i);
-                }
-            } else {
-                if (0 != mark.size() % 2) {
+            if (str1Char[i] != str2Char[i - symbolCount])
+            {
+                if (0 == mark.size() % 2)
+                {
                     mark.add(i);
                 }
             }
-            if (i - symbolCount == str2Char.length - 1) {
-                if (0 == mark.size() % 2 && i + 1 < str1Char.length) {
-                    mark.add(i + 1);
+            else
+            {
+                if (0 != mark.size() % 2)
+                {
+                    mark.add(i);
+                }
+            }
+            if (i - symbolCount == str2Char.length - 1)
+            {
+                if (0 == mark.size() % 2 && i + 1 < str1Char.length)
+                {
+                    //存在非符号，才把后面标出来
+                    if (!"".equals(removeSymbol(str1.substring(i + 1))))
+                    {
+                        mark.add(i + 1);
+                    }
                 }
                 break;
             }
         }
-        if (0 != mark.size() % 2) {
-            mark.add(str1Char.length - 1);
+        if (0 != mark.size() % 2)
+        {
+            mark.add(str1Char.length);
         }
         return mark;
     }
@@ -63,7 +81,8 @@ public class 比对两句话 {
      * @param input
      * @return
      */
-    public static String removeSymbol(String input) {
+    public static String removeSymbol(String input)
+    {
         return input.replaceAll("\\p{P}|\\p{S}", "");
     }
 }
