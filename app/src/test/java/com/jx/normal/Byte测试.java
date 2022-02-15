516b38681818ad32c171fb79181863af10bbacbd
jx <jixiao@guiji.ai>
2022-02-15 17:43:38 +0800

    test

--- app/src/test/java/com/jx/normal/Byte测试.java
+++ app/src/test/java/com/jx/normal/Byte测试.java
@@ -10,15 +10,29 @@ import org.junit.Test;
  * @author Administrator
  *
  */
-public class Byte测试
-{
+public class Byte测试 {
 
     @Test
-    public void Byte测试()
-    {
+    public void Byte测试() {
         int i1 = 103;
         System.out.println(Integer.SIZE);
         System.out.println(i1 & 0x0000000F);
+
+
+        byte a1 = -30;
+        System.out.println(Integer.toHexString(a1));
+        byte a2 = -116;
+        System.out.println(Integer.toHexString(a2));
+        byte a3 = -88;
+        System.out.println(Integer.toHexString(a3));
+
+        byte a4 = -17;
+        System.out.println(Integer.toHexString(a4));
+        byte a5 = -72;
+        System.out.println(Integer.toHexString(a5));
+        byte a6 = -113;
+        System.out.println(Integer.toHexString(a6));
+
     }
 
 }
