af129a5e47f32f479379a6aad019eb90b83c3e58
jx <jixiao@guiji.ai>
2022-04-22 18:33:34 +0800

    音频播放

--- app/src/main/java/com/jx/androiddemo/testactivity/function/f31to40/f32/F32Activity.java
+++ app/src/main/java/com/jx/androiddemo/testactivity/function/f31to40/f32/F32Activity.java
@@ -1,11 +1,16 @@
 package com.jx.androiddemo.testactivity.function.f31to40.f32;
 
 import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.View;
 
 import com.jx.androiddemo.BaseMvpActivity;
 import com.jx.androiddemo.R;
 import com.jx.androiddemo.testactivity.empty.EmptyContract;
 import com.jx.androiddemo.testactivity.empty.EmptyPresenter;
+import com.jx.androiddemo.tool.ClickListenerUtil;
+import com.jx.androiddemo.tool.ToastUtil;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +18,8 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class F32Activity extends BaseMvpActivity<EmptyPresenter> implements EmptyContract.View {
+
+    private MediaPlayer mPlayer;
 
     @Override
     protected void initInject() {
@@ -32,6 +39,33 @@ public class F32Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
     }
 
     private void initView() {
+        initMediaPlayer();
+    }
+
+    private void initMediaPlayer() {
+        mPlayer = new MediaPlayer();
+        mPlayer.setOnPreparedListener(mp -> {
+            Log.d(TAG, "准备好了");
+        });
+        mPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                Log.d(TAG, "缓冲进度：" + percent);
+            }
+        });
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Log.d(TAG, "播放完成");
+
+            }
+        });
+        mPlayer.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener() {
+            @Override
+            public void onSeekComplete(MediaPlayer mp) {
+                Log.d(TAG, "onSeekComplete");
+            }
+        });
     }
 
     @SuppressLint("CheckResult")
@@ -51,5 +85,42 @@ public class F32Activity extends BaseMvpActivity<EmptyPresenter> implements Empt
                 .subscribe(o ->
                 {
                 });*/
+    }
+
+    public void onClick(View view) {
+        if (!ClickListenerUtil.canClick()) return;
+        switch (view.getId()) {
+            case R.id.tv_load:
+                ToastUtil.showTextToast("load");
+                try {
+                    mPlayer.setDataSource("https://guiyu-tici.oss-cn-shanghai.aliyuncs.com/tici/149-2-20220411173608441.wav");
+                    mPlayer.prepare();
+                } catch (Exception e) {
+                    Log.d(TAG, e.getMessage());
+                }
+                break;
+            case R.id.tv_play:
+                ToastUtil.showTextToast("play");
+                mPlayer.start();
+                break;
+            case R.id.tv_pause:
+                ToastUtil.showTextToast("pause");
+                mPlayer.pause();
+                break;
+            case R.id.tv_resume:
+                ToastUtil.showTextToast("resume");
+                mPlayer.start();
+                break;
+            case R.id.tv_stop:
+                ToastUtil.showTextToast("stop");
+                mPlayer.stop();
+                mPlayer.reset();
+                break;
+            case R.id.tv_release:
+                ToastUtil.showTextToast("release");
+                mPlayer.release();
+                initMediaPlayer();
+                break;
+        }
     }
 }
