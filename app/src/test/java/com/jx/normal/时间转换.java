2bfd45d0951c7a40469654eaf4371c4ac4dc9d1c
jx <jixiao@guiji.ai>
2022-05-10 17:06:40 +0800

    note

---
+++ app/src/test/java/com/jx/normal/时间转换.java
@@ -0,0 +1,14 @@
+package com.jx.normal;
+
+import org.junit.Test;
+
+public class 时间转换 {
+    @Test
+    public void main() {
+        int i1 = 105;
+        int i2 = 115;
+        int i3 = 111;
+        int i4 = 109;
+        System.out.println((i1 * 256 * 256 * 256 + i2 * 256 * 256 + i3 * 256 + i4) + "");
+    }
+}
